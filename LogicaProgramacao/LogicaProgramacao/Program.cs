f8a274322ec07a2ab9c6e99364b65922bb7ccd31
Josaf4 <josafa.soares2909@gmail.com>
2024-05-22 01:37:34 -0400

    101
    1010

---
+++ LogicaProgramacao/LogicaProgramacao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LogicaProgramacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            double saldo = 10.98989898;
+
+            Console.WriteLine(n1 + " == " + n2);
+            Console.WriteLine(saldo.ToString("F2"));
+
+            int a = 10;
+
+            a += 5;
+            Console.WriteLine(a);
+
+        }
+    }
+}

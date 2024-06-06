d027cc34e3b8875335eb1077e3ee83bb0002dc86
Josaf4 <josafa.soares2909@gmail.com>
2024-06-06 02:43:14 -0400

    101
    1010

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Maria");
+            list.Add("João");
+            list.Insert(2, "KKK");
+            list.Remove("KKK");
+
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("List Count: " + list.Count);
+        }
+    }
+}
---
+++ TopicsSpecial/TopicsSpecial/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TopicsSpecial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch-case
+            int x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1: 
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    Console.WriteLine("1");
+                    break;
+                case 3:
+                    Console.WriteLine("1");
+                    break;
+                case 4:
+                    Console.WriteLine("1");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Value.");
+                    break;
+
+            }
+
+
+            //Data e Hora
+            DateTime d1 = new DateTime(2024, 05, 15, 20, 40, 6);
+            DateTime d2 =DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+
+            TimeSpan t1 = new TimeSpan(0, 1, 30, 15);
+            Console.WriteLine(t1);
+
+            //Formato UTC
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d4 = DateTime.Now;
+            Console.WriteLine(d3);
+            Console.WriteLine(d4.ToUniversalTime());
+
+            //Padrão ISO 8601
+
+        }
+    }
+}

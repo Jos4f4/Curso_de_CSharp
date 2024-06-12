522addabc35df993d31a78109807f8b470ff4c98
Josaf4 <josafa.soares2909@gmail.com>
2024-06-11 21:11:22 -0400

    101
    1010

--- Composition/Composition/obj/Debug/net8.0/Composition.AssemblyInfo.cs
+++ Composition/Composition/obj/Debug/net8.0/Composition.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Composition")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d027cc34e3b8875335eb1077e3ee83bb0002dc86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb29683fa57944a8aa53bf2200c567719dc0f969")]
 [assembly: System.Reflection.AssemblyProductAttribute("Composition")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Composition")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

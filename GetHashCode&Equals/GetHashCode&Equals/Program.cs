2f18cf7f7061ab3215a9d0c2aa250d33866a211b
Josaf4 <josafa.soares2909@gmail.com>
2024-07-03 14:49:42 -0400

    101
    1010

---
+++ GetHashCode&Equals/GetHashCode&Equals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GetHashCode_Equals.entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente a = new Cliente { Name = "Maria", Email = "Maria@gmmail.com" };
+            Cliente b = new Cliente { Name = "Maria", Email = "Maria@gmmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
